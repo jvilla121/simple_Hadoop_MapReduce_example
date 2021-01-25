f459eed9d955b73fb5402ed7e0fc960581116e8f
jvilla121 <jvilla121@outlook.com>
2021-01-25 02:35:38 +0000

    lowercase text remove stopwords & remove punctuation

--- mapper.py
+++ mapper.py
@@ -3,12 +3,16 @@ import sys
 
 # get all lines from stdin
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+    # remove leading and trailing whitespace & lowercase all
+    line = line.strip().lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
-    # output tuples (word, 1) in tab-delimited format
+    # output tuples (word, 1) in tab-delimited format & removes punctuation + omits stopwords
+    from sklearn.feature_extraction import stop_words
+    stopwords = set(stop_words.ENGLISH_STOP_WORDS)
+    stops = set([';','.',',',':','?','!'])
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stopwords and word not in stops:
+                print '%s\t%s' % (word, "1")
